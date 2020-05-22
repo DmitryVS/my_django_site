070c5a9b183dd01439fa23cf92da75fd40853d94
DmitryVS <treekbar@outlook.com>
2020-05-23 02:34:26 +0500

    end

--- nasledie/urls.py
+++ nasledie/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, re_path 
 from . import views
+from .views import PostDetailView
 
 urlpatterns = [
-	re_path(r'^$', views.post_list, name='post_list'),
+	re_path(r'^$', views.index, name='index'),
+	re_path(r'^nasledie/$', views.post_list, name='post_list'),
+	re_path(r'^nasledie/block/(?P<pk>[0-9]+)/$', PostDetailView.as_view(), name='block-detail'),
 	re_path(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- nasledie/views.py
+++ nasledie/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .forms import PostForm
 from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
+from .models import Block
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.order_by('created_date')
-	return render(request, 'nasledie/post_list.html', {'posts': posts})
-
+	blocks = Block.objects.order_by('created_date')
+	return render(request, 'nasledie/post_list.html', {'posts': posts, 'blocks': blocks})
+def index(request):
+	blocks = Block.objects.order_by('created_date')
+	return render(request, 'nasledie/index.html', {'blocks': blocks})
+def list_block(request):
+	posts = Post.objects.order_by('created_date')
+	blocks = Block.objects.order_by('created_date')
+	return render(request, 'nasledie/list_block.html', {'posts': posts,'blocks': blocks})
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -20,3 +29,17 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html',{'form': form})	
+class PostDetailView(DetailView):
+
+    queryset = Post.objects.all()
+
+    def get_object(self):
+        # Call the superclass
+        object = super(PostDetailView, self).get_object()
+        # Record the last accessed date
+        object.last_accessed = timezone.now()
+        object.save()
+        date=[]
+        date.append(object)
+        # Return the object
+        return object
